089b93eb83652cac1ff2518ea8a0b67af60e6691
szymonkacprowicz <szymonkrzysztof11@gmail.com>
2023-02-28 15:14:20 +0100

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,239 +1,265 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme:
+            GoogleFonts.ptSansTextTheme().apply(bodyColor: Colors.grey[200]),
+      ),
       home: Scaffold(
-        backgroundColor: Colors.grey[400],
         appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
           title: Text("Weather app v1"),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                      child: Text(
-                        "Poznań, PL",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 34),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "${DateTime.now().day}.${DateTime.now().month}.${DateTime.now().year}",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
-                ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                HSLColor.fromAHSL(1, 214, 0.89, 0.61).toColor(),
+                HSLColor.fromAHSL(1, 214, 0.89, 0.21).toColor()
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                    padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                    child: CircleAvatar(
-                      radius: 55,
-                      backgroundImage: NetworkImage(
-                          "https://w7.pngwing.com/pngs/487/562/png-transparent-sun-logo-sunlight-silhouette-thumbnail.png"),
-                    )),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                          child: Text(
-                            "21°C",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 55),
-                          ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+                        child: Text(
+                          "Poznań, PL",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 34),
                         ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(10, 0, 0, 5),
-                          child: Text(
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "${DateTime.now().day}.${DateTime.now().month}.${DateTime.now().year}",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                      child: Icon(
+                        Icons.sunny,
+                        size: 100,
+                      )),
+                  Column(
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          Text(
+                            "21°",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 60,
+                                color: Colors.grey[200]),
+                          ),
+                          Text(
                             "Mostly suny",
                             style: TextStyle(fontSize: 18),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Divider(
-              height: 40,
-              thickness: 3,
-              color: Color.fromARGB(255, 147, 147, 147),
-              indent: 20,
-              endIndent: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "28°C",
-                        style: TextStyle(fontSize: 25),
+                        ],
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("temp_max")),
-                          ],
+                    ],
+                  ),
+                ],
+              ),
+              Divider(
+                height: 40,
+                thickness: 3,
+                color: Color.fromARGB(255, 147, 147, 147),
+                indent: 5,
+                endIndent: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "28°C",
+                          style: TextStyle(fontSize: 25),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "7mph",
-                        style: TextStyle(fontSize: 25),
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("wind")),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "05:55",
-                        style: TextStyle(fontSize: 25),
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("temp_max")),
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("sunrise")),
-                          ],
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "7mph",
+                          style: TextStyle(fontSize: 25),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "18°C",
-                        style: TextStyle(fontSize: 25),
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("temp_min")),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "0%",
-                        style: TextStyle(fontSize: 25),
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("wind")),
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("rain")),
-                          ],
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "05:55",
+                          style: TextStyle(fontSize: 25),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                      child: Text(
-                        "21:37",
-                        style: TextStyle(fontSize: 25),
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                                child: Text("sunset")),
-                          ],
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("sunrise")),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "18°C",
+                          style: TextStyle(fontSize: 25),
                         ),
-                      ],
-                    ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("temp_min")),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "0%",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("rain")),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: Text(
+                          "21:37",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                                  child: Text("sunset")),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(
+                height: 40,
+                thickness: 3,
+                color: Color.fromARGB(255, 147, 147, 147),
+                indent: 5,
+                endIndent: 5,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 5),
+                child: Row(
+                  children: [
+                    Text("Next 5 days"),
                   ],
                 ),
-              ],
-            ),
-            Divider(
-              height: 40,
-              thickness: 3,
-              color: Color.fromARGB(255, 147, 147, 147),
-              indent: 20,
-              endIndent: 20,
-            ),
-          ],
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                child: Text(
+                  "0%",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ));
